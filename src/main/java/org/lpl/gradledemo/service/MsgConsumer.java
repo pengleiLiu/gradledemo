f0658699459e9f4902fd02b326752ace3dcff54f
liupenglei <liupenglei@xingshulin.com>
2019-04-30 18:08:11 +0800

    add kafka

---
+++ src/main/java/org/lpl/gradledemo/service/MsgConsumer.java
@@ -0,0 +1,17 @@
+package org.lpl.gradledemo.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MsgConsumer {
+
+
+  private final static String TOPIC = "a";
+
+  @KafkaListener(topics = {TOPIC})
+  public void processMessage(String content) {
+
+    System.out.println("消息被消费" + content);
+  }
+}
